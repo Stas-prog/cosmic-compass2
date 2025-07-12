8e34d064728098b8c14458bac2b9bc1b89ebac45
Stas-prog <skubav86@gmail.com>
2025-07-12 23:40:12 +0300

    2 commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // <-- тимчасово ігнорує всі ESLint помилки при збірці
+  },
 };
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true, // <-- тимчасово ігнорує всі ESLint помилки при збірці
+//   },
+// };
+
+// module.exports = nextConfig;
+
 
 export default nextConfig;
