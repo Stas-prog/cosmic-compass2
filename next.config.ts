b845a5e6c046ee70c95151e8890f3ecce842bb23
Stas-prog <skubav86@gmail.com>
2026-01-11 19:38:25 +0200

    26 commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // <-- тимчасово ігнорує всі ESLint помилки при збірці
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.json$/,
